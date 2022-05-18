148cd9476e908bee67fc67ccb9264e786bad2f57
alexandrexpj <alexandrexpjr@hotmail.com>
2022-05-18 16:44:51 -0300

    feat: implementa classe `App`

---
+++ app/back-end/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+    this.app.use(accessControl);
+    this.app.use(express.json);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => {
+      console.log(`Ouvindo a porta ${PORT}`);
+    });
+  }
+}
+
+export const { app } = new App();
