b9fa7e964a64dd050e10d4af2946640ec006aae8
alexandrexpj <alexandrexpjr@hotmail.com>
2022-05-19 14:32:12 -0300

    feat: implementa métodos `addTask`, `changeTask` e `deleteTask`

--- app/back-end/src/services/TaskService.ts
+++ app/back-end/src/services/TaskService.ts
@@ -5,5 +5,22 @@ export class TaskService {
   public getTasks = async ():Promise<ITask[]> => {
     const tasks = await TaskModel.findAll();
     return tasks as ITask[];
+  };
+
+  public addTask = async (task: ITask) => {
+    const createdTask = await TaskModel.create({ ...task });
+    return createdTask.id;
+  };
+
+  public changeTask = async (task: ITask, id: number) => {
+    const updatedTask = await TaskModel.update({ ...task },
+      { where: { id } }
+    );
+    return updatedTask;
+  };
+
+  public deleteTask = async (id: number) => {
+    const deletedTask = await TaskModel.destroy({ where: { id } });
+    return deletedTask;
   }
 }
