5d7e3aa427bd4495a69519056a637880a3643404
alexandrexpj <alexandrexpjr@hotmail.com>
2022-05-20 15:42:19 -0300

    feat: desenvolve botão para excluir tarefa

--- app/front-end/src/components/Task.jsx
+++ app/front-end/src/components/Task.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
-import { formatDate } from '../helpers';
-import { changeStatus } from '../services/requestAPI';
+import React, { useContext, useState } from 'react';
+import myContext from '../context/myContext';
+import { formatDateTime } from '../helpers';
+import { changeStatus, deleteTask } from '../services/requestAPI';
 
 function Task(props) {
   const [ currentStatus, setStatus ] = useState('');
   const { task } = props;
   const { title, status, createdAt, id } = task;
+  const { setTasks } = useContext(myContext);
 
   const handleChange = async ({ target }) => {
     setStatus(target.value);
     await changeStatus(`/task/${id}`, { status: target.value });
+    if (currentStatus === 'Pronto') {
+      await removeTask();
+    };
   };
+
+  const removeTask = async () => {
+    await deleteTask(`/task/${id}`);
+    setTasks(previous => previous - 1);
+  }
+
+  const { date, time } = formatDateTime(createdAt);
   return (
     <div className='task'>
       <p> { title } </p>
@@ -19,7 +31,8 @@ function Task(props) {
         <option value="em andamento">Em andamento</option>
         <option value="pronto">Pronto</option>
       </select>
-      <p> { formatDate(createdAt).date } - { formatDate(createdAt).time } </p>
+      <p> { time } - { date } </p>
+      <button type='buttom' onClick={ removeTask } className="delete-task-btn"> Excluir </button>
     </div>
   );
 }
